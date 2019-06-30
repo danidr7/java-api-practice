f958bf567e4bf9a2076e3260d175d2c26b92ca24
Daniel da Rosa <danieldarosati@gmail.com>
2019-06-30 19:00:41 -0300

    get saiyajins

--- src/main/java/br/com/danidr7/controller/SaiyajinController.java
+++ src/main/java/br/com/danidr7/controller/SaiyajinController.java
@@ -5,11 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 
 @RestController
 @RequestMapping("saiyajin")
@@ -20,16 +21,21 @@ public class SaiyajinController {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    @GetMapping
-    public String getSaiyajins() {
-        return "Saiyajins!";
-    }
-
-    @PostMapping("/create")
+    @PostMapping
     public void createSaiyajin(@RequestBody @Valid Saiyajin saiyajin) {
         logger.info("saving new saiyajin");
 
         mongoTemplate.save(saiyajin, "saiyajin");
+    }
+
+    @GetMapping
+    public Saiyajin getSaiyajins(@RequestParam Map<String, String> params) {
+        logger.info("saving new saiyajin");
+        Query query = new Query();
+        query.addCriteria(Criteria.where("name").is("Eric"));
+        Saiyajin saiyajin = (Saiyajin) mongoTemplate.find(query, Saiyajin.class);
+
+        return saiyajin;
     }
 
 }
--- src/main/java/br/com/danidr7/repository/SaiyajinRepository.java
+++ src/main/java/br/com/danidr7/repository/SaiyajinRepository.java
@@ -3,8 +3,10 @@ package br.com.danidr7.repository;
 import br.com.danidr7.domain.Saiyajin;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface SaiyajinRepository extends MongoRepository<Saiyajin, String> {
 
-
+    public List<Saiyajin> findByName(String name);
 
 }
